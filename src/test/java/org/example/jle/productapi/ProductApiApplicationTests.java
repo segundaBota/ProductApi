bec8274f7d8f8769423749a0598dc76ba379a080
Jose Luis España García-Hidalgo <joseespana@capitole-consulting.com>
2026-02-16 09:37:30 +0100

    First commit. Empty project

---
+++ src/test/java/org/example/jle/productapi/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jle.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
